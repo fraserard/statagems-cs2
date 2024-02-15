92828b5ba458d90da650e45874b26c6ee5534503
Fraser Ard <fraserard@gmail.com>
2024-02-15 01:47:07 -0700

    add admin seed

--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,18 +1,36 @@
 import { Client } from "@planetscale/database";
 import * as dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
+
+import { getPlayersFromSteam } from "~/server/helpers/steam";
 import * as schema from "./schema";
 
-dotenv.config({ path: "./.env" });
+async function main() {
+  await seedAdmin();
+  await seedMaps();
+}
 
-export const db = drizzle(
-  new Client({
-    url: process.env.DATABASE_URL,
-  }).connection(),
-  { schema }
-);
+async function seedAdmin() {
+  console.log("Seeding admin");
 
-await seedMaps();
+  await db.delete(schema.player);
+
+  const steamId = process.env.ADMIN_STEAM_ID;
+
+  const admin = (
+    await getPlayersFromSteam(process.env.STEAM_API_KEY!, [steamId!])
+  )[0];
+
+  await db.insert(schema.player).values({
+    username: admin!.steamUsername,
+    steamId: BigInt(admin!.steamId),
+    steamUsername: admin!.steamUsername,
+    steamAvatarHash: admin!.steamAvatarHash,
+    role: "admin",
+  });
+
+  console.log("Seeding admin complete");
+}
 
 async function seedMaps() {
   console.log("Seeding maps");
@@ -59,3 +77,14 @@ async function seedMaps() {
 
   console.log("Seeding maps complete");
 }
+
+dotenv.config({ path: "./.env" });
+
+const db = drizzle(
+  new Client({
+    url: process.env.DATABASE_URL,
+  }).connection(),
+  { schema }
+);
+
+await main();
